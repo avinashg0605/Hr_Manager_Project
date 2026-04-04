9c3a424912c84f8fd3fb1b13fe9f94142436714b
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-04-04 18:27:27 +0530

    terraform vpc

--- aws-3tier-terraform/output.tf
+++ aws-3tier-terraform/output.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = module.vpc.vpc_id
 }
 
 output "public_subnet_1_id" {
-  value = aws_subnet.public_subnet_1.id
+  value = module.vpc.public_subnet_1_id
 }
 
 output "public_subnet_2_id" {
-  value = aws_subnet.public_subnet_2.id
+  value = module.vpc.public_subnet_2_id
 }
