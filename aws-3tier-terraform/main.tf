fb8164104cc25efa774c0ebfec85c2f7f560431d
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-04-04 18:11:27 +0530

    terraform vpc

--- aws-3tier-terraform/main.tf
+++ aws-3tier-terraform/main.tf
@@ -11,42 +11,42 @@ module "sg" {
   vpc_id = module.vpc.vpc_id
 }
 
-module "ec2" {
-  source = "./modules/ec2"
+# module "ec2" {
+#   source = "./modules/ec2"
   
-  vpc_id                = module.vpc.vpc_id
+#   vpc_id                = module.vpc.vpc_id
   
-  public_subnet_1_id    = module.vpc.public_subnet_1_id
+#   public_subnet_1_id    = module.vpc.public_subnet_1_id
   
-  bastion_server_config = [
-    var.bastion_server_config.instance_type,
-    var.bastion_server_config.ami_id,
-    var.bastion_server_config.ebs_volume_size
-  ]
-    alb_sg = module.sg.alb_sg
-    public_ec2_sg = module.sg.public_ec2_sg
-    private_ec2_sg = module.sg.private_ec2_sg
+#   bastion_server_config = [
+#     var.bastion_server_config.instance_type,
+#     var.bastion_server_config.ami_id,
+#     var.bastion_server_config.ebs_volume_size
+#   ]
+#     alb_sg = module.sg.alb_sg
+#     public_ec2_sg = module.sg.public_ec2_sg
+#     private_ec2_sg = module.sg.private_ec2_sg
   
-  web_ec2 =[
-      var.web_ec2.name,
-      var.web_ec2.ami,
-      var.web_ec2.instance_type,
-      var.web_ec2.availability_zone,
-      var.web_ec2.key_pair,
-      var.web_ec2.ebs_volume_size,
-      var.web_ec2.tags             
-  ]
+#   web_ec2 =[
+#       var.web_ec2.name,
+#       var.web_ec2.ami,
+#       var.web_ec2.instance_type,
+#       var.web_ec2.availability_zone,
+#       var.web_ec2.key_pair,
+#       var.web_ec2.ebs_volume_size,
+#       var.web_ec2.tags             
+#   ]
 
-  api_ec2 =[
-      var.api_ec2.name,
-      var.api_ec2.ami,
-      var.api_ec2.instance_type,
-      var.api_ec2.availability_zone,
-      var.api_ec2.key_pair,
-      var.api_ec2.ebs_volume_size,
-      var.api_ec2.tags             
-  ]
-}
+#   api_ec2 =[
+#       var.api_ec2.name,
+#       var.api_ec2.ami,
+#       var.api_ec2.instance_type,
+#       var.api_ec2.availability_zone,
+#       var.api_ec2.key_pair,
+#       var.api_ec2.ebs_volume_size,
+#       var.api_ec2.tags             
+#   ]
+# }
 
 # module "alb" {
 #   source = "./modules/alb"
