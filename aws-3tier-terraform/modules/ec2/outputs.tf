20f385e0725bd2593a6299af457addd3792f660b
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-04-08 16:42:34 +0530

    terraform vpc

--- aws-3tier-terraform/modules/ec2/outputs.tf
+++ aws-3tier-terraform/modules/ec2/outputs.tf
@@ -1,27 +1,27 @@
-output "bastion_instance_id" {
-  description = "ID of the Bastion host instance"
-  value       = aws_instance.bastion.id
-}
-
-output "bastion_public_ip" {
-  description = "Public IP of the Bastion host"
-  value       = aws_instance.bastion.public_ip
-}
-
-output "bastion_private_ip" {
-  description = "Private IP of the Bastion host"
-  value       = aws_instance.bastion.private_ip
-}
-
-# output "bastion_sg_id" {
-#   description = "Security Group ID attached to Bastion"
-#   value       = aws_security_group.bastion_sg.id
+# output "bastion_instance_id" {
+#   description = "ID of the Bastion host instance"
+#   value       = aws_instance.bastion.id
 # }
 
-# output "instance_id" {
-#   value = aws_instance.this.id
+# output "bastion_public_ip" {
+#   description = "Public IP of the Bastion host"
+#   value       = aws_instance.bastion.public_ip
 # }
 
-# output "public_ip" {
-#   value = aws_instance.this.public_ip
+# output "bastion_private_ip" {
+#   description = "Private IP of the Bastion host"
+#   value       = aws_instance.bastion.private_ip
 # }
+
+# # output "bastion_sg_id" {
+# #   description = "Security Group ID attached to Bastion"
+# #   value       = aws_security_group.bastion_sg.id
+# # }
+
+# # output "instance_id" {
+# #   value = aws_instance.this.id
+# # }
+
+# # output "public_ip" {
+# #   value = aws_instance.this.public_ip
+# # }
