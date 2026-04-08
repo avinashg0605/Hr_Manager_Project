e173a784fa7f3a9b9268f2360995a92c9f3902e9
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-04-08 19:20:06 +0530

    terraform vpc

--- aws-3tier-terraform/output.tf
+++ aws-3tier-terraform/output.tf
@@ -17,3 +17,10 @@ output "public_subnet_2_id" {
 # output "ec2_public_ips" {
 #   value = { for name, mod in module.ec2_instances : name => mod.public_ip }
 # }
+
+output "web_instance_ids_map" {
+  value = {
+    for key, web_instance in module.web :
+    key => web_instance.instance_ids
+  }
+}
