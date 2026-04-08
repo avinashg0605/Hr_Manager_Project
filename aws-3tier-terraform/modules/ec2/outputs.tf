d829e33d8a98694cbd783d453a82d88ca9a17340
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-04-08 14:06:14 +0530

    terraform vpc

--- aws-3tier-terraform/modules/ec2/outputs.tf
+++ aws-3tier-terraform/modules/ec2/outputs.tf
@@ -18,10 +18,10 @@ output "bastion_private_ip" {
 #   value       = aws_security_group.bastion_sg.id
 # }
 
-output "instance_id" {
-  value = aws_instance.this.id
-}
+# output "instance_id" {
+#   value = aws_instance.this.id
+# }
 
-output "public_ip" {
-  value = aws_instance.this.public_ip
-}
+# output "public_ip" {
+#   value = aws_instance.this.public_ip
+# }
